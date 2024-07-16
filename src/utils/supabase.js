5f63704061b13c97570593e84611dc15846a81cc
Yusubin <99532840+Yusubin@users.noreply.github.com>
2024-07-16 16:56:42 +0900

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wwaiihcqhjpqlprvfpok.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3YWlpaGNxaGpwcWxwcnZmcG9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAxMzgyNTAsImV4cCI6MjAzNTcxNDI1MH0.Wtdi-DZJdz4yyqG9RLtt-KuRRUrFWYuEjlhWwtw2XeQ';
+const supabaseUrl = '#';
+const supabaseKey = '#';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
