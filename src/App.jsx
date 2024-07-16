88dff12124bdbbffb7c4a5834c4c64ea386576f4
Yusubin <99532840+Yusubin@users.noreply.github.com>
2024-07-16 17:01:52 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import supabase from './utils/supabase';
+import { useState } from 'react'
+import Home from './component/Home'
+import Login from './component/Login'
+
+import SignUP from './component/SignUP'
+
+import './App.css'
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      // Example: Fetch data from 'posts' table
-      let { data: posts, error } = await supabase.from('posts').select('*');
-      if (error) console.log('Error fetching posts:', error);
-      else setData(posts);
-    };
-
-    fetchData();
-  }, []);
+  
 
   return (
-    <div>
-      <h1>Posts</h1>
-      <ul>
-        {data.map(post => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    <>
+    
+    <Login />
+   
+    
+    </>
+  )
 }
 
-export default App;
+export default App
