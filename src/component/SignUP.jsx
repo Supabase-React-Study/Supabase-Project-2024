7789400b127e08fe65bcf32ca4e3d549f9d63d2f
Yusubin <99532840+Yusubin@users.noreply.github.com>
2024-07-16 17:00:05 +0900

    [PRACTICE]
    DD

---
+++ src/component/SignUP.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const SignUP =()=>{
+
+  return (
+    <>
+    
+    <div>
+    <p>email:</p><input placeholder=' email'></input>
+    
+    </div>
+      
+      <p>password:</p><input placeholder='password 入力  '></input>
+      <p>password 確認:</p><input placeholder='passwordW 確認 '></input>
+      <button>Sign UP</button>
+    </>
+  )
+
+}
+
+export default SignUP;
