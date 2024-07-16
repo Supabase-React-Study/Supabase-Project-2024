69db0336a0ebef75503175334b04cae44a789c8a
Yusubin <99532840+Yusubin@users.noreply.github.com>
2024-07-16 16:54:26 +0900

    [DB] supabase 連結
     supabase 連結

---
+++ src/utils/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://wwaiihcqhjpqlprvfpok.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3YWlpaGNxaGpwcWxwcnZmcG9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAxMzgyNTAsImV4cCI6MjAzNTcxNDI1MH0.Wtdi-DZJdz4yyqG9RLtt-KuRRUrFWYuEjlhWwtw2XeQ';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
