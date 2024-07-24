37718321bc36e6baefdf81e4739430ec1b2503ab
YS.Kim <139199198+Yeseul0411@users.noreply.github.com>
2024-07-24 13:48:18 +0900

    signup1

---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignUp from '../../components/singup/SignUp'
+// import { login, signup } from './actions'
+
+export default function SignUpPage() {
+  return (
+    
+    <SignUp/>
+  )
+}
--- components/singup/signup.tsx
+++ components/singup/signup.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import SignUpCss from "./SignUpCss.css";
 
-export default function SignUpPage() {
+
+export default function SignUp() {
     return (
-      <form>
-        <label htmlFor="email">メールアドレス</label>
-        <input id="email" name="email" type="email" required />
-        <label htmlFor="password">パスワード</label>
-        <input id="password" name="password" type="password" required />
-        <label htmlFor="confirmpassword">パスワード確認</label>
-        <input id="confirmpassword" name="confirmpassword" type="confirmpassword" required />
-        <label htmlFor="nickname">ニックネーム</label>
-        <input id="nickname" name="nickname" type="nickname" required />
-        <input type="radio" name="gender" value="">男性</input>
-        <input type="radio" name="gender" value="">女性</input>
-        <input type="radio" name="gender" value="">その他</input>
-        <input type="checkbox" name="checkbox" value="">
-        利用規約および個人情報の取扱いについてに同意する
-        </input>
-        <button formAction={SignUpPage}>登録</button>
-      </form>
-    )
-  }
+        <section>
+            <header className="Singup-header">
+                <h2>E-mailで新規加入</h2>
+            </header>
+            <form className="user-info">
+                <div className="mail-addr">
+                    <div>メールアドレス</div>
+                    <input placeholder="abcde@gmail.com" type="email" id="email"/>
+                </div>
+                <div className="password">
+                    <div>パスワード</div>
+                    <input type="password" id="password"></input>
+                </div>
+                <div className="check-pw">
+                    <div>パスワード確認</div>
+                    <input type="check-pw" id="check-pw"></input>
+                </div>
+                <div className="nickn">
+                    <div>ニックネーム</div>
+                    <input type="nickn" id="nickn"></input>
+                </div>
+                <div className="sex">
+                    <div>性別
+                    <input type="radio" id="sex">男</input>
+                    <input type="radio" id="sex">女</input>
+                    <input type="radio" id="sex">その他</input>
+                    </div>
+                </div>
+                <div className="agree">
+                    <input type="checkbox" id="agree">
+                    利用規約および個人情報の取扱いについてに同意する。
+                    </input>
+                </div>
+                <div className="btns">
+                    <button id="signup" name="signup" value="signup">登録</button>
+                </div>
+            </form>
+        </section>
+
+    );
+}
 
  
