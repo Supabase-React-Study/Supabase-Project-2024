ca9b21b12fef2b944453c08f7b0023019a727e44
YS.Kim <139199198+Yeseul0411@users.noreply.github.com>
2024-07-26 17:07:37 +0900

    Revert "Merge branch 'yusub' into Yeseul"
    This reverts commit 498d5a56a9f98caf91f02b4954d74f1648225add, reversing
    changes made to 52cd8053fc84e9c064854a2055b25fd438d93aa6.

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -7,32 +7,24 @@ export async function GET(request: Request) {
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/'
-  console.log(code);
-  console.log(next);
+
   if (code) {
-    const supabase = createClient();
-    const { session, error } = await supabase.auth.exchangeCodeForSession(code);
-  
-    if (error) {
-      console.error('Error exchanging code for session:', error);
-      return NextResponse.redirect(`${origin}/auth/auth-code-error`);
-    }
-  
-    // Optional: Log session details for debugging
-    console.log('Session:', session);
-  
-    const forwardedHost = request.headers.get('x-forwarded-host');
-    const isLocalEnv = process.env.NODE_ENV === 'development';
-  
-    if (isLocalEnv) {
-      return NextResponse.redirect(`${origin}${next}`);
-    } else if (forwardedHost) {
-      return NextResponse.redirect(`https://${forwardedHost}${next}`);
-    } else {
-      return NextResponse.redirect(`${origin}${next}`);
+    const supabase = createClient()
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    if (!error) {
+      const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
+      const isLocalEnv = process.env.NODE_ENV === 'development'
+      if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+        return NextResponse.redirect(`${origin}${next}`)
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+      } else {
+        return NextResponse.redirect(`${origin}${next}`)
+      }
     }
   }
-  
+
   // return the user to an error page with instructions
   return NextResponse.redirect(`${origin}/auth/auth-code-error`)
 }
