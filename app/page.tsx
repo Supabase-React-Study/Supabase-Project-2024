dbb4ae8ae74fd5297130a0e8759b708a15d3af77
Yusubin <99532840+Yusubin@users.noreply.github.com>
2024-07-26 13:34:36 +0900

    [asdf]

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 
+<<<<<<< Updated upstream
 import { createClient } from "@/utils/supabase/server";
 
 export default async function Index() {
   
   return (
     <p>afsd</p>
+=======
+
+export default async function Index() {
+  
+
+
+  return (
+    <p></p>
+>>>>>>> Stashed changes
   );
 }
