00a450cb4c6f02ce770eed473778286fa3c8f903
NaGyeong <n0rkjdx0@gmail.com>
2024-07-30 16:29:42 +0900

    2024/07/30
    password 변경 완료

--- app/modifypassword/page.tsx
+++ app/modifypassword/page.tsx
@@ -19,27 +19,27 @@ export default function ModifyPassword() {
     // 사용자 정보를 가져오는함수
     async function fetchUser() {
       const { data: userData, error: userError } = await supabase.auth.getUser();
-      
+
       if (userError || !userData?.user) {
         router.push('/login'); // 로그인 페이지로 리디렉션
       } else {
         setUser(userData.user);
 
-      // 추가 정보 가져오기
-      const { data: userInfoData, error: userInfoError } = await supabase
-      .from('userinfo') // public.userInfo 테이블
-      .select('*') // 필요한 열을 선택
-      .eq('email', userData.user.email) // 사용자 이메일로 필터링
-      .single(); 
+        // 추가 정보 가져오기
+        const { data: userInfoData, error: userInfoError } = await supabase
+          .from('userinfo') // public.userInfo 테이블
+          .select('*') // 필요한 열을 선택
+          .eq('email', userData.user.email) // 사용자 이메일로 필터링
+          .single();
 
-    if (userInfoError) {
-      console.error('Error fetching user info:', userInfoError);
-    } else {
+        if (userInfoError) {
+          console.error('Error fetching user info:', userInfoError);
+        } else {
 
-      console.log('Fetched user info:', userInfoData);
-      setUserInfo(userInfoData);
-    }
-  }
+          console.log('Fetched user info:', userInfoData);
+          setUserInfo(userInfoData);
+        }
+      }
       setLoading(false);
     }
 
@@ -50,7 +50,7 @@ export default function ModifyPassword() {
     return <div>Loading...</div>;
   }
 
-    return (
-      <div><ModifyPasswordJSX userInfo={userInfo}/></div>
-    )
-  }
+  return (
+    <div><ModifyPasswordJSX userInfo={userInfo} /></div>
+  )
+}
